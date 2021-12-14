329056f105e2484ef0ec0a4df9c9f8e61d173861
Ralf Westphal <info@ralfw.de>
2021-12-14 15:22:40 +0200

    beispiel wieder vereinfachen

--- api/hello.ts
+++ api/hello.ts
@@ -1,10 +1,2 @@
-import { Application } from 'https://deno.land/x/oak@v9.0.0/mod.ts';
-
-const app = new Application();
-
-app.use((ctx) => {
-	ctx.response.body = `Hello World! ${Deno.version.deno} @ ${new Date().toLocaleTimeString()}`;
-	console.log(ctx);
-});
-
-export default app.handle;
+// @ts-ignore
+export default () => new Response(`Hello🥳, from Deno v${Deno.version.deno}! @ ${new Date().toLocaleTimeString()}`);
