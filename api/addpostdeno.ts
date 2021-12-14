f07809d67235c74e8dff93e600a431d899d9c6f0
Ralf Westphal <info@ralfw.de>
2021-12-14 19:04:49 +0200

    ServerRequest

--- api/addpostdeno.ts
+++ api/addpostdeno.ts
@@ -1,4 +1,6 @@
-export default (req:Request) => {
+import { ServerRequest } from 'https://deno.land/std@0.89.0/http/server.ts'
+
+export default (req:ServerRequest) => {
     console.log(req);
-    return new Response(`Hello, from Deno! v${Deno.version.deno}! ${new Date().toLocaleTimeString()}`);
+    return new Response(`Hello, from Deno!! v${Deno.version.deno}! ${new Date().toLocaleTimeString()}`);
 }
