7fd819c65d1803ded23ab429243a7026a9bae15f
Ralf Westphal <info@ralfw.de>
2021-12-14 17:04:25 +0200

    ~

--- api/addpost.ts
+++ api/addpost.ts
@@ -3,6 +3,8 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 
 app.use(async (ctx) => {
+    console.log(ctx)
+    console.log("---------------")
     const operands = await ctx.request.body().value; // receive json data
     console.log(`calc with request body params: ${operands.A}+${operands.B}`)
     ctx.response.body = { Result: operands.A + operands.B }; // return json data
