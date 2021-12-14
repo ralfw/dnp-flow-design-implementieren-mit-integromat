2ce64adc2c84fbb81b1adfdf2ca6715ef18dc70f
Ralf Westphal <info@ralfw.de>
2021-12-14 14:59:42 +0200

    hello mit zeitstempel

--- api/hello.ts
+++ api/hello.ts
@@ -3,7 +3,7 @@ import { Application } from 'https://deno.land/x/oak@v9.0.0/mod.ts';
 const app = new Application();
 
 app.use((ctx) => {
-	ctx.response.body = 'Hello World!';
+	ctx.response.body = `Hello World! ${Deno.version.deno} @ ${new Date().toLocaleTimeString()}`;
 	console.log(ctx);
 });
 
