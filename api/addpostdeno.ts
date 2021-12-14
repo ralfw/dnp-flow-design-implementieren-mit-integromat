da4f75c357404fce61c143311368c21c8349cb3b
Ralf Westphal <info@ralfw.de>
2021-12-14 18:00:10 +0200

    trying again a simple server

--- api/addpostdeno.ts
+++ api/addpostdeno.ts
@@ -1,5 +1,3 @@
-import { Response, Request } from 'https://deno.land/std@0.106.0/http/server.ts';
-
 export default (req:Request) => {
     console.log(req);
     return new Response(`Hello, from Deno! v${Deno.version.deno}!`);
