cc647f7db6e65b156b0931c36e167e899d1abc2b
Ralf Westphal <info@ralfw.de>
2021-12-14 15:52:16 +0200

    lokalen server hinzugefügt

--- api/hello.ts
+++ api/hello.ts
@@ -3,8 +3,10 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 
 app.use((ctx) => {
+    console.log(`rquest received! ${ctx.request}`)
     ctx.response.body = `Hello World with Oak! @ ${new Date().toLocaleTimeString()}`;
-    console.log(ctx);
 });
+
+export function serve(port:number) { app.listen({port:port})}
 
 export default app.handle;
---
+++ main.ts
@@ -0,0 +1,4 @@
+import { serve } from "./api/hello.ts"
+
+console.log("Serving on http://localhost:8000")
+serve(8000);
